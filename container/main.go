413ee2d43ebcc17579f00b823b9c37b12a5039f7
ruanjiancheng <ruanjiancheng@bytedance.com>
2023-02-24 16:48:21 +0800

    modify container

--- container/main.go
+++ container/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "[v1] Hello, Kubernetes!")
+	host, _ := os.Hostname()
+	dbURL := os.Getenv("DB_URL")
+	io.WriteString(w, fmt.Sprintf("[v4] Hello, Kubernetes! From host: %s, Get Database Connect URL: %s", host, dbURL))
 }
 
 func main() {
